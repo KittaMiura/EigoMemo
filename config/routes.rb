2dc06828f5ebace0908af8c0f4415c85ffb4886d
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-12-06 10:08:39 +0000

    [Add]post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'
@@ -10,5 +11,6 @@ Rails.application.routes.draw do
     root :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
