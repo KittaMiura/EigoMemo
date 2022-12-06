09d58037ee5893fa38e8f8c23d4dcc598eefacb6
Kitta Miura <roppou8818@gmail.com>
2022-12-06 19:55:42 +0000

    [Add]deviseユーザー編集

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "signup", :to => "users/registrations#new"
+    root :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+  
+  scope module: :users do
+    get '/users/:id/profile' => 'users#show', as: 'profile'
+    resources :users, only: [:edit, :update]
+    resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
