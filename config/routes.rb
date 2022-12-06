607600b1787377a2e1b583c1d53b78016710ff06
Kitta Miura <roppou8818@gmail.com>
2022-12-06 20:16:51 +0000

    [Add]diary作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     get '/users/:id/profile' => 'users#show', as: 'profile'
     resources :users, only: [:edit, :update]
     resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    resources :diaries
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_06_100537) do
+ActiveRecord::Schema.define(version: 2022_12_06_201401) do
+
+  create_table "diaries", force: :cascade do |t|
+    t.string "title"
+    t.text "english"
+    t.text "japanese"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
