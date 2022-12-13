71de4497df8a58e77f1f61fa9eaf03e075b49039
Kitta Miura <roppou8818@gmail.com>
2022-12-13 16:39:08 +0000

    [Add]flashmessage

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'net-smtp'
+
+gem 'devise-i18n'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  
+  validates :spell, presence: true
+  validates :meaning, presence: true
+  
 end
