7d6a170654630d464a177d2d23cbf9b296bd4c28
Kitta Miura <roppou8818@gmail.com>
2022-12-15 08:11:52 +0000

    [Add]頭文字スペルページ

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -2,7 +2,11 @@ class Users::SearchesController < ApplicationController
   before_action :authenticate_user!
   
   def search
-    @posts = Post.looks(params[:search], params[:word])
+    @posts = Post.looks(params[:search], params[:word]).where(user_id: current_user.id).includes(:user).order(:spell)
+  end
+  
+  def spellA
+    @posts = Post.where("spell LIKE 'a%'").where(user_id: current_user.id).includes(:user).order(:spell)
   end
   
 end
